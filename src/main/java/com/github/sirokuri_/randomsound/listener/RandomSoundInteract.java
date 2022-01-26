15a22327251c75c44fd7e5dab3f10540af8498d6
46kuri_ <ghost.46kuri@gmail.com>
2022-01-26 21:16:19 +0900

    [Fix] 何のサウンドを再生しているか分かるよう修正

--- src/main/java/com/github/sirokuri_/randomsound/listener/RandomSoundInteract.java
+++ src/main/java/com/github/sirokuri_/randomsound/listener/RandomSoundInteract.java
@@ -1,10 +1,14 @@
 package com.github.sirokuri_.randomsound.listener;
 
 import com.github.sirokuri_.randomsound.RandomSound;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -31,7 +35,10 @@ public class RandomSoundInteract implements Listener {
         if ((event.getHand() != EquipmentSlot.HAND || event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
         ItemStack itemStack = player.getInventory().getItemInMainHand();
         if (itemStack.getType() == Material.STICK){
+            BaseComponent[] component = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',"&4" + sound + "&r のサウンド再生"));
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR,component);
             player.playSound(location,sound,randomValue,1);
+
         }
     }
 }
